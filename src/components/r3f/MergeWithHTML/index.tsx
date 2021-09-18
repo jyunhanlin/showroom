7008013a6fde406751abe419b72cf5c647c9a233
Jyun-Han Lin <jyunhanlin@gmail.com>
2021-09-18 10:10:45 +0800

    feat: porting three version

--- src/components/r3f/MergeWithHTML/index.tsx
+++ src/components/r3f/MergeWithHTML/index.tsx
@@ -1,107 +1,120 @@
 import styled from '@emotion/styled';
+import { useEffect } from 'react';
+
+import Sketch from './three/sketch';
 
 const isProd = process.env.NODE_ENV === 'production';
 const assetPath = isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '';
 
 export const MergeWithHTML = () => {
+  useEffect(() => {
+    new Sketch({
+      dom: document.getElementById('canvas'),
+    });
+  }, []);
+
   return (
     <Main>
-      <div className="page">
-        <header>
-          <h1>Oceans</h1>
-          <img src={`${assetPath}/img/header.jpg`} alt="" />
-        </header>
+      <div data-scroll>
+        <div className="page">
+          <header>
+            <h1>Oceans</h1>
+            <img src={`${assetPath}/img/header.jpg`} alt="" />
+          </header>
 
-        <div className="grid">
-          <div className="item item_v">
-            <div className="item__image">
-              <img src={`${assetPath}/img/1.jpg`} alt="" />
-              <div className="item__meta">December 23, 2020</div>
+          <div className="grid">
+            <div className="item item_v">
+              <div className="item__image">
+                <img src={`${assetPath}/img/1.jpg`} alt="" />
+                <div className="item__meta">December 23, 2020</div>
+              </div>
+
+              <h2 className="item__title">Octopus punches fish in the head (just because it can)</h2>
+              <p>
+                Octopuses sometimes partner with fish to hunt, but the partnership comes with risks (for the fish, that
+                is).
+              </p>
             </div>
 
-            <h2 className="item__title">Octopus punches fish in the head (just because it can)</h2>
-            <p>
-              Octopuses sometimes partner with fish to hunt, but the partnership comes with risks (for the fish, that
-              is).
-            </p>
-          </div>
+            <div className="item item_h">
+              <div className="item__image">
+                <img src={`${assetPath}/img/2.jpg`} alt="" />
+                <div className="item__meta">December 01, 2020</div>
+              </div>
 
-          <div className="item item_h">
-            <div className="item__image">
-              <img src={`${assetPath}/img/2.jpg`} alt="" />
-              <div className="item__meta">December 01, 2020</div>
+              <h2 className="item__title">
+                Newfound marine blob looks like &lsquo;party balloon&rsquo; with two strings, scientists say
+              </h2>
+              <p>This is the first species NOAA scientists have ever discovered from video footage alone.</p>
             </div>
 
-            <h2 className="item__title">
-              Newfound marine blob looks like &lsquo;party balloon&rsquo; with two strings, scientists say
-            </h2>
-            <p>This is the first species NOAA scientists have ever discovered from video footage alone.</p>
-          </div>
+            <div className="item item_h">
+              <div className="item__image">
+                <img src={`${assetPath}/img/4.jpg`} alt="" />
+                <div className="item__meta">November 26, 2020</div>
+              </div>
 
-          <div className="item item_h">
-            <div className="item__image">
-              <img src={`${assetPath}/img/4.jpg`} alt="" />
-              <div className="item__meta">November 26, 2020</div>
+              <h2 className="item__title">Swarm of eels breaks record</h2>
+              <p>
+                Before we start mining for precious metals in the darkness of the deep sea, we might try switching on
+                the light first and observing our surroundings.
+              </p>
             </div>
 
-            <h2 className="item__title">Swarm of eels breaks record</h2>
-            <p>
-              Before we start mining for precious metals in the darkness of the deep sea, we might try switching on the
-              light first and observing our surroundings.
-            </p>
-          </div>
+            <div className="item item_v">
+              <div className="item__image">
+                <img src={`${assetPath}/img/3.jpg`} alt="" />
+                <div className="item__meta">November 03, 2020</div>
+              </div>
 
-          <div className="item item_v">
-            <div className="item__image">
-              <img src={`${assetPath}/img/3.jpg`} alt="" />
-              <div className="item__meta">November 03, 2020</div>
+              <h2 className="item__title">Mantis shrimp punch down</h2>
+              <p>Home-stealers fought the hardest for smaller-than-ideal dens.</p>
             </div>
 
-            <h2 className="item__title">Mantis shrimp punch down</h2>
-            <p>Home-stealers fought the hardest for smaller-than-ideal dens.</p>
-          </div>
+            <div className="item item_v">
+              <div className="item__image">
+                <img src={`${assetPath}/img/1.jpg`} alt="" />
+                <div className="item__meta">October 05, 2020</div>
+              </div>
 
-          <div className="item item_v">
-            <div className="item__image">
-              <img src={`${assetPath}/img/1.jpg`} alt="" />
-              <div className="item__meta">October 05, 2020</div>
+              <h2 className="item__title">Megalodon&rsquo;s hugeness</h2>
+              <p>Even among its extinct relatives, Megalodon was unequalled in length and mass.</p>
             </div>
 
-            <h2 className="item__title">Megalodon&rsquo;s hugeness</h2>
-            <p>Even among its extinct relatives, Megalodon was unequalled in length and mass.</p>
-          </div>
+            <div className="item item_h">
+              <div className="item__image">
+                <img src={`${assetPath}/img/2.jpg`} alt="" />
+                <div className="item__meta">July 27, 2020</div>
+              </div>
 
-          <div className="item item_h">
-            <div className="item__image">
-              <img src={`${assetPath}/img/2.jpg`} alt="" />
-              <div className="item__meta">July 27, 2020</div>
+              <h2 className="item__title">Adorable sunfish</h2>
+              <p>Sunfish in the Molidae family are among the biggest fish in the world.</p>
             </div>
 
-            <h2 className="item__title">Adorable sunfish</h2>
-            <p>Sunfish in the Molidae family are among the biggest fish in the world.</p>
-          </div>
+            <div className="item item_h">
+              <div className="item__image">
+                <img src={`${assetPath}/img/4.jpg`} alt="" />
+                <div className="item__meta">August 18, 2020</div>
+              </div>
 
-          <div className="item item_h">
-            <div className="item__image">
-              <img src={`${assetPath}/img/4.jpg`} alt="" />
-              <div className="item__meta">August 18, 2020</div>
+              <h2 className="item__title">Massive &lsquo;Darth Vader&rsquo; sea bug</h2>
+              <p>The newly described species is one of the biggest isopods known to science.</p>
             </div>
 
-            <h2 className="item__title">Massive &lsquo;Darth Vader&rsquo; sea bug</h2>
-            <p>The newly described species is one of the biggest isopods known to science.</p>
-          </div>
+            <div className="item item_v">
+              <div className="item__image">
+                <img src={`${assetPath}/img/3.jpg`} alt="" />
+                <div className="item__meta">June 01, 2020</div>
+              </div>
 
-          <div className="item item_v">
-            <div className="item__image">
-              <img src={`${assetPath}/img/3.jpg`} alt="" />
-              <div className="item__meta">June 01, 2020</div>
+              <h2 className="item__title">Scientists capture the world&rsquo;s deepest octopus</h2>
+              <p>The octopus was found miles beneath the ocean surface.</p>
             </div>
-
-            <h2 className="item__title">Scientists capture the world&rsquo;s deepest octopus</h2>
-            <p>The octopus was found miles beneath the ocean surface.</p>
           </div>
         </div>
       </div>
+
+      <div id="canvas" />
     </Main>
   );
 };
@@ -148,11 +161,9 @@ const Main = styled.main`
   }
 
   .item {
-    /* border: 1px solid #000; */
     align-self: center;
     justify-self: center;
     text-decoration: none;
-    color: #333;
   }
   .item:nth-of-type(odd) {
     justify-self: start;
@@ -207,5 +218,14 @@ const Main = styled.main`
       width: 100%;
       max-width: none;
     }
+  }
+
+  #canvas {
+    height: 100vh;
+    width: 100vw;
+    position: fixed;
+    z-index: -1;
+    top: 0;
+    left: 0;
   }
 `;
