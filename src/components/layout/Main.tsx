d6f7bd19e1d1e96c13a82019a861650497a0b830
Jyun-Han Lin <jyunhanlin@gmail.com>
2021-10-23 11:46:05 +0800

    feat: add three.js playground

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,3 +1,3 @@
 import { Box, BoxProps } from '@chakra-ui/react';
 
-export const Main = (props: BoxProps) => <Box {...props} />;
+export const Main = (props: BoxProps) => <Box as="main" {...props} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Heading, Link as ChakraLink, UnorderedList, ListItem } from '@chakra-ui/react';
+import { Box, Heading, Link as ChakraLink, UnorderedList, ListItem } from '@chakra-ui/react';
 import Link from 'next/link';
 
 import { Container } from '@/components/layout/Container';
@@ -7,31 +7,41 @@ import { Main } from '@/components/layout/Main';
 
 const Index = () => (
   <Container height="100%">
-    <Main padding="1rem">
-      <Heading as="h3" size="md">
-        React Three Fiber playground
-      </Heading>
-      <UnorderedList spacing="4px" paddingTop="1rem">
-        <ListItem>
-          <ChakraLink as={Link} href="/particles">
-            Particles
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ChakraLink as={Link} href="/ripple-blend">
-            Ripple Blend
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ChakraLink as={Link} href="/merge-with-html">
-            Merge with HTML
-          </ChakraLink>
-          ,{' '}
-          <ChakraLink as={Link} href="/scroll-effect">
-            Scroll Effect
-          </ChakraLink>
-        </ListItem>
-      </UnorderedList>
+    <Main display="flex" padding="1rem" height="100%">
+      <Box width="50%" height="100%" overflow="auto">
+        <Heading as="h3" size="md">
+          React Three Fiber playground
+        </Heading>
+        <UnorderedList spacing="4px" paddingTop="1rem">
+          <ListItem>
+            <ChakraLink as={Link} href="/particles">
+              Particles
+            </ChakraLink>
+          </ListItem>
+          <ListItem>
+            <ChakraLink as={Link} href="/ripple-blend">
+              Ripple Blend
+            </ChakraLink>
+          </ListItem>
+          <ListItem>
+            <ChakraLink as={Link} href="/scroll-effect">
+              Scroll Effect
+            </ChakraLink>
+          </ListItem>
+        </UnorderedList>
+      </Box>
+      <Box width="50%" height="100%" overflow="auto">
+        <Heading as="h3" size="md">
+          Three.js playground
+        </Heading>
+        <UnorderedList spacing="4px" paddingTop="1rem">
+          <ListItem>
+            <ChakraLink as={Link} href="/merge-with-html">
+              Merge with HTML
+            </ChakraLink>
+          </ListItem>
+        </UnorderedList>
+      </Box>
     </Main>
     <DarkModeSwitch />
   </Container>
