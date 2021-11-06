879eb3ae427b1d4c929988a58decce514fcd30ec
Jyun-Han Lin <jyunhanlin@gmail.com>
2021-11-06 19:04:22 +0800

    feat: add tensorflow playground index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,26 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Heading, Link as ChakraLink, UnorderedList, ListItem } from '@chakra-ui/react';
+import Link from 'next/link';
 
 import { Container } from '@/components/layout/Container';
 import { DarkModeSwitch } from '@/components/layout/DarkModeSwitch';
 import { Main } from '@/components/layout/Main';
-import { NavOrAside } from '@/components/layout/NavOrAside';
 
-const Index: React.FC = () => (
+const Index = () => (
   <Container height="100%">
-    <Flex direction={['column', 'row']} padding="16px" maxWidth="1200px" margin="auto">
-      <NavOrAside />
-      <Main></Main>
-    </Flex>
+    <Main display={['box', 'flex']} padding={['3rem 1rem', '1rem']} height="100%">
+      <Box width={['100%', '50%']} height={['auto', '100%']} overflow="auto" marginBottom="2rem">
+        <Heading as="h3" size="md">
+          Tensorflow playground
+        </Heading>
+        <UnorderedList spacing="4px" paddingTop="1rem">
+          <ListItem>
+            <ChakraLink as={Link} href="/bodypix">
+              BodyPix
+            </ChakraLink>
+          </ListItem>
+        </UnorderedList>
+      </Box>
+    </Main>
     <DarkModeSwitch />
   </Container>
 );
