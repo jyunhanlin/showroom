6c15d1bdac957dcc69cc428a72aa07bd6e729b98
jyunhanlin <jyunhanlin@users.noreply.github.com>
2022-05-13 14:20:14 +0000

    deploy: c4c9e41d51f724ab9193d4ec35f65e62fb647a53

---
+++ main/_next/static/3roplRp316c18RTi_mxld/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/746-947674f4340f2542.js","static/chunks/pages/index-a07d31f925f20448.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
