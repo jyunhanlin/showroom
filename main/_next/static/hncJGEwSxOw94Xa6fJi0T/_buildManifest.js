554a31c5a250303859843b4154898f40cb509358
jyunhanlin <jyunhanlin@users.noreply.github.com>
2022-12-14 02:30:53 +0000

    deploy: 8ab91d9a0ed15a1b02f635c5fd9398af9dec15fa

---
+++ main/_next/static/hncJGEwSxOw94Xa6fJi0T/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/454-414447c547225478.js","static/chunks/pages/index-166be70113714e06.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
