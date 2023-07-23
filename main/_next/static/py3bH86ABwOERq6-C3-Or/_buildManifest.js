4e9e34180c096f877d70c5efef572b0a20b498d9
Hank Lin <jyunhanlin@gmail.com>
2023-07-23 14:57:18 +0800

    remove old branch

--- main/_next/static/py3bH86ABwOERq6-C3-Or/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/483-46f2f69217e2763e.js","static/chunks/pages/index-8b3e889d57170668.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
