70fbe8c2122da208189fd02174c933760a6bb6ed
Hank Lin <jyunhanlin@gmail.com>
2023-08-01 08:14:52 +0800

    fix: assetPath in tailwind template

--- apps/nextjs-playground/src/app/(tailwind-template)/tailwind-template/page.tsx
+++ apps/nextjs-playground/src/app/(tailwind-template)/tailwind-template/page.tsx
@@ -1,4 +1,6 @@
-import Image from 'next/image'
+import Image from 'next/image';
+
+import { assetPath } from '@/utils/assetPath';
 
 export default function Home() {
   return (
@@ -17,7 +19,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
+              src={`${assetPath}/vercel.svg`}
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -31,7 +33,7 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
+          src={`${assetPath}/next.svg`}
           alt="Next.js Logo"
           width={180}
           height={37}
@@ -86,9 +88,7 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Explore the Next.js 13 playground.</p>
         </a>
 
         <a
@@ -109,5 +109,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
